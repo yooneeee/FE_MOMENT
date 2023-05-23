f6c014d20338550e0202bac31feb252e16d89714
nminy <minyoung9328@gmail.com>
2023-05-23 21:38:29 +0900

    Feat: Styles 생성

---
+++ src/styles/ContainerStyles.js
@@ -0,0 +1,29 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100%;
+`;
+export const CenteredContent = styled.div`
+  flex: 1 0 auto;
+  margin: 0px auto;
+  max-width: 500px;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  padding: 40px 0px;
+`;
+export const TitleLogo = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 50px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+export const Title = styled.div`
+  font-size: 50px;
+  font-weight: 800;
+`;
---
+++ src/styles/InputStyles.js
@@ -0,0 +1,30 @@
+import { styled } from "styled-components";
+
+export const InputWrap = styled.div`
+  display: flex;
+  border-radius: 8px;
+  padding: 15px;
+  margin: 10px 0;
+  background-color: white;
+  border: 1px solid #e2e0e0;
+  &:focus-within {
+    border: 1px solid #636363;
+  }
+`;
+export const InputTitle = styled.div`
+  font-size: 17px;
+  font-weight: 600;
+  color: #262626;
+  margin-top: 15px;
+`;
+export const Input = styled.input`
+  width: 100%;
+  outline: none;
+  border: none;
+  height: 17px;
+  font-size: 14px;
+  font-weight: 400;
+  &::placeholder {
+    color: #cacaca;
+  }
+`;
