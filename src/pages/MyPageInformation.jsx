348c749543fa6a61148cb31963f85ac8f2cb019d
nminy <minyoung9328@gmail.com>
2023-05-25 03:21:31 +0900

    Design: 프로필 수정 UI

--- src/pages/MyPageInformation.jsx
+++ src/pages/MyPageInformation.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { styled } from "styled-components";
 
 function MyPageInformation() {
+  const [image, setImage] = useState("img/snowball.png");
+  const fileInput = useRef(null);
+
   const [pwIsVisible, setpwIsVisible] = useState(false);
   const [nickIsVisible, setnickIsVisible] = useState(false);
+  const [ImgIsVisible, setImgIsVisible] = useState(false);
 
   /* 버튼 클릭시 히든 폼 */
   const nicknameHandler = () => {
@@ -11,6 +15,9 @@ function MyPageInformation() {
   };
   const pwHandler = () => {
     setpwIsVisible(!pwIsVisible);
+  };
+  const imgHandler = () => {
+    setImgIsVisible(!ImgIsVisible);
   };
 
   /* 취소 버튼 클릭시 되돌아가기 */
@@ -20,19 +27,87 @@ function MyPageInformation() {
   const pwcancelHandler = () => {
     setpwIsVisible(false);
   };
+  const imgcancelHandler = () => {
+    setImgIsVisible(false);
+    setImage("img/snowball.png");
+  };
+  const basicImgHandler = () => {
+    setImage("img/snowball.png");
+  };
+
+  const fileSelectHandler = (e) => {
+    const file = e.target.files[0];
+    // 파일 처리 로직 추가
+    // 이미지 업로드 후 이미지 변경 로직
+    const reader = new FileReader();
+    reader.onload = () => {
+      const uploadedImage = reader.result;
+      setImage(uploadedImage);
+    };
+    reader.readAsDataURL(file);
+  };
 
   return (
     <Container>
-      <div>기본 회원정보</div>
-      <Line />
-      <Text>사진</Text>
+      <Box>
+        <Title>
+          기본 회원정보<span>필수</span>
+        </Title>
+        <Line />
+        <Text1>사진</Text1>
+        <ProfileContainer>
+          <ProfileImg
+            src={image}
+            alt="프로필 이미지"
+            //   onClick={() => {
+            //     fileInput.current.click();
+            //   }}
+          />
+        </ProfileContainer>
+        <TextColumn>
+          <ProfileText>
+            {ImgIsVisible ? (
+              <HiddenForm>
+                {/* <HiddenNick> */}
+                {/* <button>사진 선택</button> */}
+                <UploadButton>
+                  사진선택
+                  <input type="file" onChange={fileSelectHandler}></input>
+                </UploadButton>
+                <BasicImgButton onClick={basicImgHandler}>
+                  기본이미지로 변경
+                </BasicImgButton>
+                {/* </HiddenNick> */}
+                <ButtonColumn>
+                  <HiddenFormBtn onClick={imgcancelHandler}>취소</HiddenFormBtn>
+                  <HiddenFormBtn>완료</HiddenFormBtn>
+                </ButtonColumn>
+              </HiddenForm>
+            ) : (
+              <>
+                <span>회원님을 알릴 수 있는 사진을 등록해 주세요.</span>
+                <br />
+                <span>
+                  등록 된 사진은 회원님의 게시물이나 피드에 사용됩니다.
+                </span>
+              </>
+            )}
+          </ProfileText>
+        </TextColumn>
+        {ImgIsVisible ? null : <Button onClick={imgHandler}>사진 변경</Button>}
+      </Box>
       <Line1 />
       <Box>
         <Text>닉네임</Text>
-        <Text2>
+        <TextColumn>
           {nickIsVisible ? (
             <HiddenForm>
-              <HiddenInput type="text" />
+              <HiddenNick>
+                <span>길이는 최대 ~자 이내로 작성해주세요.</span>
+                <br />
+                <span>중복 닉네임 불가합니다.</span>
+              </HiddenNick>
+              <HiddenInput type="text" placeholder="닉네임 입력(최대 ~자)" />
               <ButtonColumn>
                 <HiddenFormBtn onClick={nickCancelHandler}>취소</HiddenFormBtn>
                 <HiddenFormBtn>완료</HiddenFormBtn>
@@ -41,7 +116,7 @@ function MyPageInformation() {
           ) : (
             <span>미뇽</span>
           )}
-        </Text2>
+        </TextColumn>
         {nickIsVisible ? null : (
           <Button onClick={nicknameHandler}>닉네임 변경</Button>
         )}
@@ -51,7 +126,7 @@ function MyPageInformation() {
         <Text>
           <span>비밀번호</span>
         </Text>
-        <Text2>
+        <TextColumn>
           {pwIsVisible ? (
             <HiddenForm>
               <Column>
@@ -74,7 +149,7 @@ function MyPageInformation() {
           ) : (
             <span>********</span>
           )}
-        </Text2>
+        </TextColumn>
         {pwIsVisible ? null : (
           <Button onClick={pwHandler}>비밀번호 변경</Button>
         )}
@@ -93,6 +168,54 @@ const Container = styled.div`
   max-width: 800px;
   margin: 10px auto 0;
 `;
+const Title = styled.div`
+  font-size: 25px;
+  font-weight: 700;
+  margin-top: 20px;
+
+  span {
+    margin-left: 10px;
+    color: #afafaf;
+    font-size: 25px;
+  }
+`;
+const ProfileContainer = styled.div`
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  overflow: hidden;
+  cursor: pointer;
+`;
+const ProfileImg = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+const ProfileText = styled.div`
+  margin-top: 10px;
+  font-weight: 600;
+`;
+const UploadButton = styled.label`
+  display: inline-block;
+  padding: 15px 25px;
+  background-color: #ffffff;
+  color: #000000;
+  border: 1px #b3b3b3 solid;
+  /* border-radius: 4px; */
+  cursor: pointer;
+
+  &:hover {
+    background-color: #e0e0e0;
+  }
+
+  input[type="file"] {
+    display: none;
+  }
+`;
+const BasicImgButton = styled(UploadButton)`
+  margin-left: 5px;
+`;
+
 const Box = styled.div`
   /* width: 100%;
   align-items: center; */
@@ -105,6 +228,9 @@ const Box = styled.div`
 const HiddenForm = styled.div`
   padding: 10px;
 `;
+const HiddenNick = styled.div`
+  margin-bottom: 10px;
+`;
 const Column = styled.div`
   display: flex;
   justify-content: space-between;
@@ -112,8 +238,15 @@ const Column = styled.div`
 `;
 
 const HiddenInput = styled.input`
+  width: 100%;
+  max-width: 55%;
   border: 1px solid #cacaca;
-  padding: 5px 30px;
+  padding: 5px 10px;
+  outline: none;
+
+  &::placeholder {
+    color: #cacaca;
+  }
 `;
 
 const ButtonColumn = styled.div`
@@ -134,20 +267,24 @@ const Text = styled.div`
   float: left;
   width: 100%;
   max-width: 30%;
-  background-color: #c0f10d;
+  /* background-color: #c0f10d; */
   height: auto;
   padding: 15px 0;
   box-sizing: border-box;
   border-top: 1px solid #f1f1f1;
   border-bottom: none;
   font-size: 14px;
+  font-weight: 600;
   text-align: left;
 `;
-const Text2 = styled.div`
+const Text1 = styled(Text)`
+  height: 30vh;
+`;
+const TextColumn = styled.div`
   display: inline-block;
   width: 100%;
   max-width: 55%;
-  background-color: #ffb773;
+  /* background-color: #ffb773; */
   height: auto;
   padding: 15px 0;
   box-sizing: border-box;
