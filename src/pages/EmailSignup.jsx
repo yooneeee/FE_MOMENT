3090209d66a7aaee768b697a599b6d712e3cc58e
yooneeee <sjy4489@gmail.com>
2023-05-24 22:47:06 +0900

    Feat: Signup 기능구현중

--- src/pages/EmailSignup.jsx
+++ src/pages/EmailSignup.jsx
@@ -1,19 +1,62 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import { styled } from "styled-components";
 import { useNavigate } from "react-router-dom";
-import {
-  Container,
-  CenteredContent,
-  TitleLogo,
-  Title,
-} from "../styles/ContainerStyles";
+import { Container, TitleLogo, Title } from "../styles/ContainerStyles";
 import { InputWrap, Input, InputTitle } from "../styles/InputStyles";
-
+import { useForm } from "../hooks/useForm";
+import { useFileReader } from "../hooks/useFileReader";
 function EmailSignup() {
   const navigate = useNavigate();
-
   const backButtonHandler = () => {
     navigate("/main");
+  };
+
+  const initialState = {
+    nickname: "",
+    role: "",
+    sex: "",
+    email: "",
+    password: "",
+    phone: "",
+    profileImg: "",
+  };
+
+  // 폼 데이터 입력값 받는 Hook
+  const [form, handleFormChange, handleFileChange, resetForm] =
+    useForm(initialState);
+  const { email, password, nickname, role, sex, phone, profileImg } = form;
+
+  // 이미지 URL 리더 Hook
+  const [imageUrl, fileReader] = useFileReader();
+
+  // 정규식
+  const emailRegex =
+    /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+  const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/;
+
+  // 이메일 에러 메세지
+  const emailMessage = useMemo(() => {
+    if (email && !emailRegex.test(email)) {
+      return "이메일을 확인해 주세요";
+    } else {
+      return "";
+    }
+  }, [email]);
+
+  // 패스워드 에러메세지
+  const passwordMessage = useMemo(() => {
+    if (password && !passwordRegex.test(password)) {
+      return "비밀번호는 영문, 숫자 포함 8자리 이상입니다.";
+    } else {
+      return "";
+    }
+  }, [password]);
+
+  // 이미지 파일 리더
+  const imageFileReader = async () => {
+    if (profileImg) {
+      fileReader(profileImg);
+    }
   };
 
   return (
@@ -24,25 +67,57 @@ function EmailSignup() {
         </TitleLogo>
         <InputTitle>닉네임</InputTitle>
         <InputWrap>
-          <Input type="text" placeholder="닉네임을 입력해주세요." />
+          <Input
+            type="text"
+            name="nickname"
+            value={nickname || ""}
+            placeholder="닉네임을 입력해주세요."
+            onChange={handleFormChange}
+          />
         </InputWrap>
         <InputTitle>직업?</InputTitle>
         <ButtonContainer>
-          <SelectionButton>모델</SelectionButton>
-          <SelectionButton>작가</SelectionButton>
+          <SelectionButton onClick={() => handleFormChange("role", "model")}>
+            모델
+          </SelectionButton>
+          <SelectionButton
+            onClick={() => handleFormChange("role", "photographer")}
+          >
+            작가
+          </SelectionButton>
         </ButtonContainer>
+
         <InputTitle>성별</InputTitle>
         <ButtonContainer>
-          <SelectionButton>남자</SelectionButton>
-          <SelectionButton>여자</SelectionButton>
+          <SelectionButton onClick={() => handleFormChange("sex", "male")}>
+            남자
+          </SelectionButton>
+          <SelectionButton onClick={() => handleFormChange("sex", "female")}>
+            여자
+          </SelectionButton>
         </ButtonContainer>
+
         <InputTitle>이메일</InputTitle>
         <InputWrap>
-          <Input type="text" placeholder="이메일 주소를 입력해주세요" />
+          <Input
+            type="text"
+            name="email"
+            value={email || ""}
+            onChange={handleFormChange}
+            message={emailMessage}
+            placeholder="이메일 주소를 입력해주세요"
+          />
         </InputWrap>
         <InputTitle>비밀번호</InputTitle>
         <InputWrap>
-          <Input type="password" placeholder="비밀번호를 입력해주세요" />
+          <Input
+            type="password"
+            name="password"
+            value={password}
+            placeholder="비밀번호를 입력해주세요"
+            onChange={handleFormChange}
+            message={passwordMessage}
+          />
         </InputWrap>
         <InputTitle>비밀번호 확인</InputTitle>
         <InputWrap>
@@ -59,6 +134,16 @@ function EmailSignup() {
 }
 
 export default EmailSignup;
+
+const CenteredContent = styled.form`
+  flex: 1 0 auto;
+  margin: 0px auto;
+  max-width: 500px;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  padding: 40px 0px;
+`;
 
 /* 버튼 선택 */
 const ButtonContainer = styled.div`
