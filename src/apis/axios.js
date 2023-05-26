086e92e2e92ba4b414feb032d9680b9d1093e8cd
yooneeee <sjy4489@gmail.com>
2023-05-27 01:44:48 +0900

    Chore: CORS 설정

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -2,8 +2,6 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_SERVER,
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-  },
+  withCredentials: true,
 });
 export { instance };
