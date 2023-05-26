36944cc971c022a51761bc7e01b1371f5d2584d6
yooneeee <sjy4489@gmail.com>
2023-05-26 16:24:53 +0900

    Feat: 이메일 인증 기능 API 추가

--- src/apis/auth/signup.js
+++ src/apis/auth/signup.js
@@ -1,13 +1,38 @@
 import { instance } from "../axios";
 // 회원가입API, method : post, url : /users/signup
-const signup = async (newUser) => {
+const signupAxios = async (newUser) => {
   try {
     const response = await instance.post(`/users/signup`, newUser);
     console.log(response);
     return response.data;
   } catch (error) {
-    console.log(error);
-    return Promise.reject(error.response.data.message);
+    const errorMessage = error.response.data.errorMessage;
+    alert(errorMessage);
+    throw error;
   }
 };
-export { signup };
+// 이메일 인증코드 보내기API, method: post, url: /emails/auth
+const sendEmailAxios = async (email) => {
+  console.log("이메일", email);
+  try {
+    const response = await instance.post("/emails/auth", email);
+    return response.data;
+  } catch (error) {
+    const errorMessage = error.response.data.errorMessage;
+    alert(errorMessage);
+    throw error;
+  }
+};
+// 이메일 인증코드 확인API, method: post, url: /emails/check
+const checkEmailAxios = async ({ email, code }) => {
+  try {
+    const response = await instance.post("/emails/check", { email, code });
+    return response.data;
+  } catch (error) {
+    const errorMessage = error.response.data.errorMessage;
+    alert(errorMessage);
+    throw error;
+  }
+};
+
+export { signupAxios, sendEmailAxios, checkEmailAxios };
