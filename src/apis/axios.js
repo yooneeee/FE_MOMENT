04e1376335054a661c3605f92318bb4345cd91ca
yooneeee <sjy4489@gmail.com>
2023-05-29 17:28:51 +0900

    Fix: 로그인시 토큰 로컬스토리지에 저장으로 변경

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -11,10 +11,10 @@ const instance = axios.create({
 instance.interceptors.request.use(
   (config) => {
     if (config.headers === undefined) return config;
-    const Access_key = sessionStorage.getItem("Access_key");
-    const Refresh_key = sessionStorage.getItem("Refresh_key");
+    const Access_key = localStorage.getItem("Access_key");
+    const Refresh_key = localStorage.getItem("Refresh_key");
 
-    if (Access_key && Refresh_key) {
+    if (Access_key || Refresh_key) {
       config.headers["Access_key"] = Access_key;
       config.headers["Refresh_key"] = Refresh_key;
     }
@@ -24,11 +24,10 @@ instance.interceptors.request.use(
     return Promise.reject(error);
   }
 );
-
+//토큰저장
 instance.interceptors.response.use(function (response) {
-  sessionStorage.setItem("Access_key", response.headers.get("Access_key"));
-  sessionStorage.setItem("Refresh_key", response.headers.get("Refresh_key"));
-
+  localStorage.setItem("Access_key", response.headers.get("Access_key"));
+  localStorage.setItem("Refresh_key", response.headers.get("Refresh_key"));
   return response;
 });
 export { instance };
