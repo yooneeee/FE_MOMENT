09816f9c8b385461cc40ac1b21955a839eb26bd6
nminy <minyoung9328@gmail.com>
2023-05-29 11:24:47 +0900

    Feat: 타입=버튼 추가

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -73,7 +73,7 @@ function Login() {
           />
         </InputWrap>
         <ButtonWrap>
-          <EmailButton onClick={loginButtonHandler}>
+          <EmailButton type="button" onClick={loginButtonHandler}>
             <ButtonText>이메일로 로그인하기</ButtonText>
           </EmailButton>
         </ButtonWrap>
