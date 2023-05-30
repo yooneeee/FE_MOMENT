817617150876729c7be273a0f40a266114ce7d9d
yooneeee <sjy4489@gmail.com>
2023-05-30 23:22:10 +0900

    Design: 로그인 비활성화 버튼 CSS 변경

--- src/apis/auth/login.js
+++ src/apis/auth/login.js
@@ -3,7 +3,7 @@ import { instance } from "../axios";
 const loginAxios = async ({ email, password }) => {
   try {
     const response = await instance.post("/users/login", { email, password });
-    return response;
+    return response.data;
   } catch (error) {
     return Promise.reject(error.response.data.message);
   }
