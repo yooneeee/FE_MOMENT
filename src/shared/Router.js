3b8b808a420f77e5776870b4fab36210864fbaae
KimHeeJun <khj981116@gmail.com>
2023-06-01 23:44:05 +0900

    Feat: 마이페이지 동적 라우팅 기능 추가

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -26,9 +26,11 @@ const Router = () => {
           <Route path="emailsignup" element={<EmailSignup />} />
           <Route path="feeds" element={<Feed />} />
           <Route path="board" element={<Board />} />
-          <Route path="feeddetail" element={<FeedDetail />} />
           <Route path={`page/:hostId`} element={<MyPage />} />
-          <Route path={`mypageinformation/:hostId`} element={<MyPageInformation />} />
+          <Route
+            path={`mypageinformation/:hostId`}
+            element={<MyPageInformation />}
+          />
           <Route path="chat" element={<Chat />} />
         </Routes>
       </Layout>
