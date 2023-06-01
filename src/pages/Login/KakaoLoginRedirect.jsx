f9eb6f8985f593750b310214c9ee3cea3156c723
yooneeee <63416696+JiyoonSon@users.noreply.github.com>
2023-06-01 15:10:10 +0900

    Feat: 로그인 성공시 리덕스로 유저 상태관리

--- src/pages/Login/KakaoLoginRedirect.jsx
+++ src/pages/Login/KakaoLoginRedirect.jsx
@@ -2,19 +2,34 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useQuery } from "react-query";
 import authKakaoLogin from "../../apis/auth/authKakaoLogin";
+import { useDispatch } from "react-redux";
+import { loginSuccess, setUser } from "../../redux/modules/user";
+import LoadingSpinner from "../../components/LoadingSpinner";
 
 function KakaoLoginRedirect() {
   const navigate = useNavigate();
   const code = new URL(window.location.href).searchParams.get("code");
-
+  const dispatch = useDispatch();
   const { data, isError } = useQuery("authKakaoLogin", async () => {
-    const result = await authKakaoLogin(code);
-    console.log(code);
-    return result;
+    try {
+      const result = await authKakaoLogin(code);
+      return result;
+    } catch (error) {
+      throw new Error("Kakao login failed");
+    }
   });
   const redirectHandler = () => {
     if (data) {
       alert("로그인 성공");
+      dispatch(loginSuccess());
+      dispatch(
+        setUser({
+          nickName: data.nickName,
+          profileImg: data.profileImg,
+          role: data.role,
+          userId: data.userId,
+        })
+      );
       navigate("/main");
     } else if (isError) {
       alert("로그인 실패");
@@ -25,7 +40,7 @@ function KakaoLoginRedirect() {
   useEffect(() => {
     redirectHandler();
   }, [data]);
-  return <div>RedirectKakaoLogin</div>;
+  return <LoadingSpinner />;
 }
 
 export default KakaoLoginRedirect;
