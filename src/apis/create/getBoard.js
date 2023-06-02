9e86e0e4e48b33a0125c4862b9d0e4e33bf39e00
nminy <minyoung9328@gmail.com>
2023-06-02 19:40:01 +0900

    Fix: 임시코드

--- src/apis/create/getBoard.js
+++ src/apis/create/getBoard.js
@@ -4,7 +4,7 @@ const getBoard = async () => {
   try {
     const { data } = await instance.get("/boards");
     console.log(data.content);
-    return data.content;
+    return data.modelboard.content;
   } catch (error) {
     throw error;
   }
