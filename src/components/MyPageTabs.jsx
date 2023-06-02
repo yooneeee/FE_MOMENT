3e166ec39587094b610dac2d0ace98301c69273d
nminy <minyoung9328@gmail.com>
2023-06-02 19:41:51 +0900

    Feat: 버튼마다 페이지 이동 Design: 마이페이지 탭 UI구현 및 버튼 클릭 시 색 변경

--- src/components/MyPageTabs.jsx
+++ src/components/MyPageTabs.jsx
@@ -1,22 +1,83 @@
 import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router";
 import styled from "styled-components";
 
 function MyPageTabs() {
-  const [isButtonClicked, setIsButtonClicked] = useState();
+  const { hostId } = useParams();
+  // console.log("첫번째", hostId);
 
-  const underlineButton = () => {
-    // 이전 상태 값을 가져와서 반전시키는 업데이트
-    setIsButtonClicked((prevState) => !prevState);
+  const navigate = useNavigate();
+  const [entireClicked, setEntireClicked] = useState(true);
+  const [feedClicked, setFeedClicked] = useState();
+  const [boardClicked, setBoardClicked] = useState();
+  const [chatClicked, setChatClicked] = useState();
+
+  const underlineButton = (buttonName) => {
+    switch (buttonName) {
+      case "entire":
+        setEntireClicked(true);
+        setFeedClicked(false);
+        setBoardClicked(false);
+        setChatClicked(false);
+        break;
+      case "feed":
+        setEntireClicked(false);
+        setFeedClicked(true);
+        setBoardClicked(false);
+        setChatClicked(false);
+        break;
+      case "board":
+        setEntireClicked(false);
+        setFeedClicked(false);
+        setBoardClicked(true);
+        setChatClicked(false);
+        break;
+      case "chat":
+        setEntireClicked(false);
+        setFeedClicked(false);
+        setBoardClicked(false);
+        setChatClicked(true);
+        break;
+      default:
+        break;
+    }
   };
+
+  const entireHandler = () => {
+    underlineButton("entire");
+    navigate(`/page/${hostId}`);
+  };
+  const feedHandler = () => {
+    underlineButton("feed");
+    navigate(`/mypagefeed/${hostId}`);
+  };
+  const boardHandler = () => {
+    underlineButton("board");
+    navigate(`/mypageboard/${hostId}`);
+  };
+  const chatHandler = () => {
+    underlineButton("chat");
+  };
+
   return (
     <TabsStyles>
       <MaueBar>
-        <TabButton isClicked={isButtonClicked} onClick={underlineButton}>
+        <TabButton isClicked={entireClicked} onClick={entireHandler}>
           전체보기
         </TabButton>
-        <TabButton>내 피드</TabButton>
-        <TabButton>게시글</TabButton>
-        <TabButton>채팅</TabButton>
+        <TabButton
+          isClicked={feedClicked}
+          isFeedClicked={feedClicked}
+          onClick={feedHandler}
+        >
+          내 피드
+        </TabButton>
+        <TabButton isClicked={boardClicked} onClick={boardHandler}>
+          내 게시글
+        </TabButton>
+        <TabButton isClicked={chatClicked} onClick={chatHandler}>
+          채팅목록
+        </TabButton>
       </MaueBar>
     </TabsStyles>
   );
@@ -45,10 +106,18 @@ const MaueBar = styled.div`
 `;
 const TabButton = styled.button`
   margin-right: 15px;
+  flex: 1;
+  max-width: 100px;
   padding: 11px;
   border: none;
   outline: none;
   background: none;
   text-decoration: none;
-  border-bottom: ${(props) => (props.isClicked ? "1px solid black" : "none")};
+  /* border-bottom: ${(props) =>
+    props.isClicked ? "2px solid black" : "none"}; */
+  font-weight: ${(props) =>
+    props.isClicked || props.isFeedClicked ? "900" : "none"};
+  color: ${(props) =>
+    props.isClicked || props.isFeedClicked ? "#ff0000" : "none"};
+  /* text-decoration: ${(props) => (props.isClicked ? "underline" : "none")}; */
 `;
