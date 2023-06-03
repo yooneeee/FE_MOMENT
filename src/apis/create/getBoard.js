41e33c08dec3e65083844bf8af5a98b6860c1d8f
KimHeeJun <khj981116@gmail.com>
2023-06-04 01:08:31 +0900

    Fix: 조건부 게시판 조회 로직 추가

--- src/apis/create/getBoard.js
+++ src/apis/create/getBoard.js
@@ -1,10 +1,14 @@
 import { instance } from "../axios";
 
-const getBoard = async () => {
+const getBoard = async (role) => {
   try {
     const { data } = await instance.get("/boards");
-    console.log(data.content);
-    return data.content;
+    console.log(data);
+    if (role === "Model") {
+      return data.modelBoard.content;
+    } else if (role === "Photographer") {
+      return data.photographerBoard.content;
+    }
   } catch (error) {
     throw error;
   }
