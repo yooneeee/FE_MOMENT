f5ac8f8530ee0bc6d44988c8f6b7380cdc3df9df
nminy <minyoung9328@gmail.com>
2023-06-04 01:05:15 +0900

    Feat: 사진삭제 기능 구현

--- src/pages/MyPageFeed.jsx
+++ src/pages/MyPageFeed.jsx
@@ -1,17 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import MyPageTabs from "../components/MyPageTabs";
 import { useParams, Link } from "react-router-dom";
-import { useQuery } from "react-query";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 import { mypage } from "../apis/mypage/mypage";
+import { mypageFeedDelete } from "../apis/mypage/mypage";
+import { FiSettings } from "react-icons/fi";
+import { BiDownArrow } from "react-icons/bi";
 
 function MyPageFeed() {
-  const { hostId } = useParams();
-  console.log(hostId);
+  const { hostId, photoId } = useParams();
+  const queryClient = useQueryClient();
+
+  const [editButtons, setEditButtons] = useState([]);
 
   const { isError, isLoading, data } = useQuery(["mypage", mypage], () =>
     mypage(hostId)
   );
+
+  /* Delete 서버 */
+  const deleteMutation = useMutation(mypageFeedDelete, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(["mypage", mypage]);
+      alert("삭제 완료!");
+      setEditButtons([]);
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+
+  const deleteButtonHandler = (photoId) => {
+    try {
+      deleteMutation.mutate(photoId);
+      toggleButtonClose(photoId);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    if (deleteMutation.isSuccess) {
+      queryClient.refetchQueries(["mypage", mypage]);
+    }
+  }, [deleteMutation.isSuccess, queryClient]);
 
   if (isLoading) {
     return <h1>로딩 중입니다(oﾟvﾟ)ノ</h1>;
@@ -20,7 +50,26 @@ function MyPageFeed() {
   if (isError) {
     return <h1>오류(⊙ˍ⊙)</h1>;
   }
-  console.log(data);
+  console.log("mypagefeed", data);
+  console.log("photo", data.photoList);
+
+  /* 토글버튼 */
+  const toggleButtonOpen = (index) => {
+    const updatedEditButtons = [...editButtons];
+    updatedEditButtons[index] = true;
+    setEditButtons(updatedEditButtons);
+  };
+
+  const toggleButtonClose = (index) => {
+    const updatedEditButtons = [...editButtons];
+    updatedEditButtons[index] = false;
+    setEditButtons(updatedEditButtons);
+  };
+
+  /* 삭제, 수정 버튼 */
+  const modifyButton = () => {
+    alert("수정");
+  };
 
   return (
     <>
@@ -47,16 +96,39 @@ function MyPageFeed() {
               </StFlex>
             </ProfileInfo>
           </ProfileSection>
-          {/* <Container> */}
           <WorkSection>
             <Work>나의 작업물</Work>
             <WorkList>
               {data.photoList.map((item, index) => {
-                return <WorkItem key={index} src={item.photoUrl} />;
+                return (
+                  <WorkItem key={index} src={item.photoUrl}>
+                    <EditButton
+                      onClick={(e) => {
+                        if (editButtons[index]) {
+                          toggleButtonClose(index);
+                        } else {
+                          toggleButtonOpen(index);
+                        }
+                      }}
+                    >
+                      <FiSettings size={18} />
+                      <BiDownArrow size={18} />
+                    </EditButton>
+                    {editButtons[index] && (
+                      <ToggleWriteMenu>
+                        <Button onClick={modifyButton}>수정</Button>
+                        <Button
+                          onClick={() => deleteButtonHandler(item.photoId)}
+                        >
+                          삭제
+                        </Button>
+                      </ToggleWriteMenu>
+                    )}
+                  </WorkItem>
+                );
               })}
             </WorkList>
           </WorkSection>
-          {/* </Container> */}
         </ContentContainer>
       </PageContainer>
     </>
@@ -64,6 +136,48 @@ function MyPageFeed() {
 }
 
 export default MyPageFeed;
+
+const ToggleWriteMenu = styled.div`
+  position: absolute;
+  top: 88px;
+  left: 25px;
+  transform: translate(-50%, -50%);
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    top: 105px;
+    right: 120px;
+  }
+`;
+const EditButton = styled.button`
+  position: absolute;
+  top: 25px;
+  left: 30px;
+  transform: translate(-50%, -50%);
+  display: none;
+
+  background-color: #ffffff;
+  border: none;
+  border-radius: 8px;
+  font-weight: 900;
+
+  padding: 8px;
+`;
+const Button = styled.button`
+  /* display: block; */
+  /* width: 100%; */
+  width: 100px;
+  margin-left: 58px;
+  padding: 8px;
+  margin-bottom: 4px;
+  background-color: #ffffff;
+  border: none;
+  border-radius: 8px;
+  font-weight: 900;
+`;
 
 const PageContainer = styled.div`
   width: 100%;
@@ -74,9 +188,7 @@ const PageContainer = styled.div`
   background-color: #f5f5f5;
   padding: 20px;
 `;
-const Container = styled.div`
-  width: 100%;
-`;
+
 const ContentContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -195,7 +307,7 @@ const WorkList = styled.div`
   margin-top: 16px;
 
   @media (min-width: 1200px) {
-    grid-template-columns: repeat(5, 1fr);
+    grid-template-columns: repeat(4, 1fr);
   }
 
   @media (max-width: 768px) {
@@ -215,11 +327,13 @@ const WorkItem = styled.div`
   background-position: center;
   background-repeat: no-repeat;
 
+  position: relative;
+
+  &:hover ${EditButton} {
+    display: block;
+  }
+
   @media (max-width: 480px) {
     height: 200px;
   }
-`;
-const Content = styled.div`
-  flex-grow: 1;
-  margin: 30px;
 `;
