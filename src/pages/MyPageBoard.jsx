d50925c863253301122f70aba38c353f06b908a3
nminy <minyoung9328@gmail.com>
2023-06-04 01:05:36 +0900

    Design: 불필요한 스타일 컴포넌트 삭제

--- src/pages/MyPageBoard.jsx
+++ src/pages/MyPageBoard.jsx
@@ -174,50 +174,12 @@ const StFlex = styled.div`
   padding: 10px;
 `;
 
-const WorkSection = styled.div`
-  flex-grow: 1;
-  margin: 30px;
-`;
-
 const Work = styled.h2`
   font-size: 24px;
   font-weight: bold;
   margin-bottom: 1rem;
 `;
 
-const WorkList = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
-  gap: 16px;
-  justify-items: center;
-  align-items: center;
-  margin-top: 16px;
-
-  @media (min-width: 1200px) {
-    grid-template-columns: repeat(5, 1fr);
-  }
-
-  @media (max-width: 768px) {
-    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-  }
-
-  @media (max-width: 480px) {
-    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-  }
-`;
-
-const WorkItem = styled.div`
-  width: 100%;
-  padding-top: 100%;
-  background-image: ${(props) => `url(${props.src})`};
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-
-  @media (max-width: 480px) {
-    height: 200px;
-  }
-`;
 const Content = styled.div`
   flex-grow: 1;
   margin: 30px;
