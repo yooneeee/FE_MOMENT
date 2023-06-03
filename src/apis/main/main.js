82e2da6726557cdac26e32000c37d308893cffdb
KimHeeJun <khj981116@gmail.com>
2023-06-04 01:08:58 +0900

    Test: 테스트용 console 추가

--- src/apis/main/main.js
+++ src/apis/main/main.js
@@ -3,6 +3,7 @@ import { instance } from "../axios";
 const main = async () => {
   try {
     const { data } = await instance.get("/home");
+    console.log(data);
     return data.eachRoleUsersList;
   } catch (error) {
     throw error;
