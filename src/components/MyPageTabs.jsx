50ba22c56cb876fce0a4b3ab4f98b4b40de7f0c7
nminy <minyoung9328@gmail.com>
2023-06-04 21:58:28 +0900

    Style: 사용하지 않는 import 삭제

--- src/components/MyPageTabs.jsx
+++ src/components/MyPageTabs.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { useQuery } from "react-query";
 import { useNavigate, useParams } from "react-router";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
