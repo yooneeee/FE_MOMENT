3fabd1479139370d697b6b0499524a9ca55225a2
yooneeee <63416696+JiyoonSon@users.noreply.github.com>
2023-06-05 23:04:05 +0900

    Design: 이미지 슬라이더 자동재생 추가

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -16,6 +16,8 @@ function Card({ user }) {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
     prevArrow: <PrevArrow />,
     nextArrow: <NextArrow />,
   };
