f481863340182b0ce1153a02dbe9e1475fc3db91
KimHeeJun <khj981116@gmail.com>
2023-06-06 18:56:55 +0900

    Feat: 피드 무한 스크롤 기능 추가

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,9 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useLayoutEffect } from "react";
 import styled from "styled-components";
 import FeedDetail from "../components/FeedDetail";
 import "../css/App.css";
 import { useQuery } from "react-query";
 import { getFeedAxios } from "../apis/feed/getFeedAxios";
+import { useInfiniteQuery } from "react-query";
+import { useRef } from "react";
+import { useObserver } from "../components/useObserver";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 function Feed() {
   // 모달 제어
@@ -18,10 +22,43 @@ function Feed() {
   };
 
   // 서버 통신
-  const { isLoading, isError, data } = useQuery("getFeedAxios", getFeedAxios);
+  // const { isLoading, isError, data } = useQuery("getFeedAxios", getFeedAxios);
+
+  const { isLoading, isError, data, fetchNextPage, refetch } = useInfiniteQuery(
+    "getFeedAxios",
+    getFeedAxios,
+    {
+      getNextPageParam: (lastPage) => {
+        if (lastPage.currentPage == lastPage.totalPages) {
+          return;
+        } else {
+          return lastPage.currentPage + 1;
+        }
+      },
+    }
+  );
+
+  // intersect
+  const onIntersect = ([entry]) => {
+    entry.isIntersecting && fetchNextPage();
+  };
+
+  // 화면의 바닥 ref를 위한 useRef
+
+  const bottom = useRef();
+
+  // 초기 렌더링 시 bottom이 null 값이 잡힘
+  useEffect(() => {
+    console.log(bottom.current);
+  }, [bottom]);
+
+  useObserver({
+    target: bottom,
+    onIntersect,
+  });
 
   if (isLoading) {
-    return;
+    return <LoadingSpinner />;
   }
 
   if (isError) {
@@ -29,31 +66,35 @@ function Feed() {
   }
 
   return (
-    <FeedContainer>
-      {data.map((item) => {
-        const isOpen = feedDetailOpen.includes(item.photoId);
-
-        return (
-          <React.Fragment key={item.photoId}>
-            <Cards>
-              <CardsImg
-                src={item.photoUrl}
-                onClick={() => {
-                  openFeedDetail(item.photoId);
-                }}
-              />
-            </Cards>
-            {isOpen && (
-              <FeedDetail
-                open={() => openFeedDetail(item.photoId)}
-                close={() => closeFeedDetail(item.photoId)}
-                photoId={item.photoId}
-              />
-            )}
-          </React.Fragment>
-        );
-      })}
-    </FeedContainer>
+    <>
+      <FeedContainer>
+        {data.pages
+          .flatMap((page) => page.photoList)
+          .map((item) => {
+            const isOpen = feedDetailOpen.includes(item.photoId);
+            return (
+              <React.Fragment key={item.photoId}>
+                <Cards>
+                  <CardsImg
+                    src={item.photoUrl}
+                    onClick={() => {
+                      openFeedDetail(item.photoId);
+                    }}
+                  />
+                </Cards>
+                {isOpen && (
+                  <FeedDetail
+                    open={() => openFeedDetail(item.photoId)}
+                    close={() => closeFeedDetail(item.photoId)}
+                    photoId={item.photoId}
+                  />
+                )}
+              </React.Fragment>
+            );
+          })}
+        <div ref={bottom}></div>
+      </FeedContainer>
+    </>
   );
 }
 
