f7bd48e8e4ca0e2f4c38de0655b0fe4ed3e56ec6
KimHeeJun <khj981116@gmail.com>
2023-06-07 23:18:12 +0900

    Design: 마이페이지 profile 박스 width 수정

--- src/components/MyPageProfile.jsx
+++ src/components/MyPageProfile.jsx
@@ -47,6 +47,7 @@ const MyPageProfile = () => {
 export default MyPageProfile;
 
 const ProfileSection = styled.div`
+  width: 350px;
   display: flex;
   flex-direction: column;
   align-items: center;
