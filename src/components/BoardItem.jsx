af780210ec12758ae472893ecde593f1bf8bde4e
yooneeee <sjy4489@gmail.com>
2023-06-19 21:18:02 +0900

    Refactor: 게시판전체조회시 보여지는 content 추가

--- src/components/BoardItem.jsx
+++ src/components/BoardItem.jsx
@@ -1,25 +1,43 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import { IoLocationSharp } from "react-icons/io5";
+import { BiDollarCircle } from "react-icons/bi";
+import { LuCalendarDays } from "react-icons/lu";
+import { FaPen } from "react-icons/fa";
 
 function BoardItem({ item, onClick }) {
   const [timeDifference, setTimeDifference] = useState(null);
+  // 오늘 날짜
+  const today = new Date();
+
   const getTimeDifference = (createdTime) => {
     const serverTime = new Date(createdTime);
     const currentTime = new Date();
-
     const timeDiff = Math.floor((currentTime - serverTime) / (1000 * 60)); // 분 단위로 시간 차이 계산
 
     if (timeDiff < 60) {
-      return `${timeDiff}분 전`;
+      return `${timeDiff}분 전 작성됨 `;
     } else if (timeDiff >= 60 && timeDiff < 1440) {
       const hoursDiff = Math.floor(timeDiff / 60);
-      return `${hoursDiff}시간 전`;
+      return `${hoursDiff}시간 전 작성됨 `;
     } else {
       const daysDiff = Math.floor(timeDiff / 1440);
-      return `${daysDiff}일 전`;
+      return `${daysDiff}일 전 작성됨 `;
     }
   };
 
+  const getDDay = (deadLine) => {
+    const targetDate = new Date(deadLine);
+    const timeDiff = Math.ceil((targetDate - today) / (1000 * 60 * 60 * 24)); // 일 단위로 시간 차이 계산
+
+    if (timeDiff === 0) {
+      return "D-day";
+    } else if (timeDiff > 0) {
+      return `D-${timeDiff}`;
+    } else {
+      return "마감";
+    }
+  };
   useEffect(() => {
     const diff = getTimeDifference(item.createdTime);
     setTimeDifference(diff);
@@ -27,20 +45,37 @@ function BoardItem({ item, onClick }) {
 
   return (
     <Item key={item.boardId} onClick={onClick}>
-      <ImageContainer img={item.boardImgUrl} />
+      <ImageContainer img={item.boardImgUrl}>
+        <DDayInfo isDday={getDDay(item.deadLine) === "D-day"}>
+          <p>{getDDay(item.deadLine)}</p>
+        </DDayInfo>
+      </ImageContainer>
       <ContentContainer>
         <Title>{item.title}</Title>
         <PhotographerInfo>
           <PhotographerRole>{item.role}</PhotographerRole>
           <PhotographerName>{item.nickName}</PhotographerName>
         </PhotographerInfo>
+        <LocationInfo>
+          <IoLocationSharp style={{ color: "#514073" }} />
+          <p>{item.location}</p>
+          <BiDollarCircle style={{ color: "#514073" }} />
+          <p>{item.pay}</p>
+        </LocationInfo>
+        <DeadLineInfo>
+          <LuCalendarDays style={{ color: "#514073" }} /> <p>{item.deadLine}</p>
+        </DeadLineInfo>
+
         <HashTagContainer>
           {item.tag_boardList.map((item) => {
             return <HashTag key={item.tagId}>{item.tag}</HashTag>;
           })}
         </HashTagContainer>
-        <MeetInfo>
-          <CreatedDate>{getTimeDifference(item.createdTime)}</CreatedDate>
+        <MeetInfo hasHashTags={item.tag_boardList.length > 0}>
+          <CreatedDate>
+            {/*             <FaPen /> */}
+            {getTimeDifference(item.createdTime)}
+          </CreatedDate>
         </MeetInfo>
       </ContentContainer>
     </Item>
@@ -70,6 +105,7 @@ const Item = styled.div`
 `;
 
 const ImageContainer = styled.div`
+  position: relative;
   width: 200px;
   height: 200px;
   margin-right: 20px;
@@ -85,7 +121,6 @@ const ImageContainer = styled.div`
 const ContentContainer = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top: 20px;
 `;
 
 const Title = styled.div`
@@ -115,7 +150,8 @@ const PhotographerRole = styled.div`
 const MeetInfo = styled.div`
   display: flex;
   align-items: center;
-  margin-top: 50px;
+
+  margin-top: ${(props) => (props.hasHashTags ? "17px" : "50px")};
   @media (max-width: 768px) {
     margin-top: 20px;
   }
@@ -138,4 +174,33 @@ const HashTag = styled.div`
   border: none;
   border-radius: 50px;
   padding: 7px;
+`;
+
+const LocationInfo = styled.div`
+  display: flex;
+  gap: 5px;
+  font-size: 16px;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+const DeadLineInfo = styled.div`
+  display: flex;
+  gap: 5px;
+  font-size: 16px;
+  align-items: center;
+`;
+const DDayInfo = styled.div`
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  z-index: 1;
+  display: flex;
+  font-size: 16px;
+  align-items: center;
+  background-color: white;
+  border-radius: 13px;
+  padding: 2px 10px;
+  opacity: 50%;
+  font-weight: 600;
+  color: ${(props) => (props.isDday ? "#ff0000" : "#000000")};
 `;
