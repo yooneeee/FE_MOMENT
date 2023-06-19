a0c87c4fc3c0224e4a3c10d45ba12fd6d60dca16
yooneeee <sjy4489@gmail.com>
2023-06-19 11:57:39 +0900

    Feat: 게시판 검색기능 추가

---
+++ src/apis/board/searchBoard.js
@@ -0,0 +1,15 @@
+import { instance } from "../axios";
+
+const searchBoardAxios = async ({ keyword, option, role }) => {
+  console.log("axios", keyword, option, role);
+  try {
+    const response = await instance.get(
+      `/boards/search?${option}=${keyword}&role=${role}`
+    );
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export { searchBoardAxios };
