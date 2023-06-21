5245f39e902b5ed763a6f129831c95864e9ebd05
nminy <minyoung9328@gmail.com>
2023-06-22 00:51:55 +0900

    Design: 속성 추가

--- src/components/MyPageProfile.jsx
+++ src/components/MyPageProfile.jsx
@@ -145,7 +145,9 @@ const ProfilePicture = styled.img`
   border-radius: 50%;
 `;
 
-const UserNickname = styled.span``;
+const UserNickname = styled.span`
+  flex-shrink: 0;
+`;
 
 const Recommend = styled.span`
   font-size: 16px;
